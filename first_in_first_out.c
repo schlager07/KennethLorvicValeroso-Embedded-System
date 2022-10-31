a1b0221ff20d50d957d7432c3b7df4c9216793b4
schlager07 <46628985+schlager07@users.noreply.github.com>
2022-10-31 17:27:44 +0800

    Update first_in_first_out.c

--- first_in_first_out.c
+++ first_in_first_out.c
@@ -21,7 +21,7 @@ void setup()
 void loop (){
 
 digitalWrite (led2, HIGH);
-delay (10000);// delay time 10s
+delay (6000);// delay time 6s
 digitalWrite (led2, LOW);
   
 digitalWrite (led3, HIGH);
@@ -29,7 +29,7 @@ delay (5000);// delay time 5s
 digitalWrite (led3, LOW);  
   
 digitalWrite (led4, HIGH);
-delay (8000);// delay time 8s
+delay (4000);// delay time 4s
 digitalWrite (led4, LOW);
   
 digitalWrite (led5, HIGH);
@@ -37,11 +37,12 @@ delay (3000);// delay time 3s
 digitalWrite (led5, LOW);
   
 digitalWrite (led6, HIGH);
-delay (1000);// delay time 1s
+delay (2000);// delay time 2s
 digitalWrite (led6, LOW);
   
 digitalWrite (led7, HIGH);
-delay (2000);// delay time 2s
+delay (1000);// delay time 1s
 digitalWrite (led7, LOW);
   
 }
+
