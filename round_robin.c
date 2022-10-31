ff96e04b600cd78195bd20e27da5602831f369be
schlager07 <46628985+schlager07@users.noreply.github.com>
2022-10-31 18:36:05 +0800

    Update round_robin.c

--- round_robin.c
+++ round_robin.c
@@ -1,65 +1,65 @@
 // Round Robin Code
 
 // int main
-int led2 = 2;
-int led3 = 3;
-int led4 = 4;
 int led5 = 5;
 int led6 = 6;
 int led7 = 7;
+int led8 = 8;
+int led9 = 9;
+int led10 = 10;
 
 void setup()
 {
 
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
   pinMode(led5, OUTPUT);
   pinMode(led6, OUTPUT);
   pinMode(led7, OUTPUT);
+  pinMode(led8, OUTPUT);
+  pinMode(led9, OUTPUT);
+  pinMode(led10, OUTPUT);
  
 }
 void loop()
 
 {
-  // delay time of all led = 1s
-  digitalWrite(led2, HIGH);
-  delay(1000); 
-  digitalWrite(led2, LOW);
-  digitalWrite(led3, HIGH);
-  delay(1000); 
-  digitalWrite(led3, LOW);
-  digitalWrite(led4, HIGH);
-  delay(1000); 
-  digitalWrite(led4, LOW);
+  // delay time of all led = 2s
   digitalWrite(led5, HIGH);
-  delay(1000); 
+  delay(2000); 
   digitalWrite(led5, LOW);
   digitalWrite(led6, HIGH);
-  delay(1000); 
+  delay(2000); 
   digitalWrite(led6, LOW);
   digitalWrite(led7, HIGH);
-  delay(1000); 
+  delay(2000); 
   digitalWrite(led7, LOW);
-  delay(1000);
+  digitalWrite(led8, HIGH);
+  delay(2000); 
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, HIGH);
+  delay(2000); 
+  digitalWrite(led9, LOW);
+  digitalWrite(led10, HIGH);
+  delay(2000); 
+  digitalWrite(led10, LOW);
+  delay(2000);
+  digitalWrite(led10, HIGH);
+  delay(2000); 
+  digitalWrite(led10, LOW);
+  digitalWrite(led9, HIGH);
+  delay(2000); 
+  digitalWrite(led9, LOW);
+  digitalWrite(led8, HIGH);
+  delay(2000); 
+  digitalWrite(led8, LOW);
   digitalWrite(led7, HIGH);
-  delay(1000); 
+  delay(2000); 
   digitalWrite(led7, LOW);
   digitalWrite(led6, HIGH);
-  delay(1000); 
+  delay(2000); 
   digitalWrite(led6, LOW);
   digitalWrite(led5, HIGH);
-  delay(1000); 
+  delay(2000); 
   digitalWrite(led5, LOW);
-  digitalWrite(led4, HIGH);
-  delay(1000); 
-  digitalWrite(led4, LOW);
-  digitalWrite(led3, HIGH);
-  delay(1000); 
-  digitalWrite(led3, LOW);
-  digitalWrite(led2, HIGH);
-  delay(1000); 
-  digitalWrite(led2, LOW);
-  delay(1000);
+  delay(2000);
 
 }
