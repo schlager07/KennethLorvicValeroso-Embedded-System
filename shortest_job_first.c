8edb8b9977fe1d5df31d942fd6c18baa5c1bcb18
schlager07 <46628985+schlager07@users.noreply.github.com>
2022-10-31 18:49:59 +0800

    Update shortest_job_first.c

--- shortest_job_first.c
+++ shortest_job_first.c
@@ -1,45 +1,45 @@
 // SJF Code
 
 // int main
-int led2 = 2;
-int led3 = 3;
-int led4 = 4;
 int led5 = 5;
 int led6 = 6;
 int led7 = 7;
+int led8 = 8;
+int led9 = 9;
+int led10 = 10;
 
 void setup()
 {
 
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
   pinMode(led5, OUTPUT);
   pinMode(led6, OUTPUT);
   pinMode(led7, OUTPUT);
+  pinMode(led8, OUTPUT);
+  pinMode(led9, OUTPUT);
+  pinMode(led10, OUTPUT);
  
 }
 void loop()
 
 {
 
+  digitalWrite(led9, HIGH);
+  delay(1500);// delay time 1.5s
+  digitalWrite(led9, LOW);
+  digitalWrite(led10, HIGH);
+  delay(2500);// delay time 2.5s
+  digitalWrite(led10, LOW);
+  digitalWrite(led8, HIGH);
+  delay(3500);// delay time 3.5s
+  digitalWrite(led8, LOW);
   digitalWrite(led6, HIGH);
-  delay(1000);// delay time 1s
+  delay(5500);// delay time 5.5s
   digitalWrite(led6, LOW);
   digitalWrite(led7, HIGH);
-  delay(2000);// delay time 2s
+  delay(6500);// delay time 6.5s
   digitalWrite(led7, LOW);
   digitalWrite(led5, HIGH);
-  delay(3000);// delay time 3s
+  delay(7500);// delay time 7.5s
   digitalWrite(led5, LOW);
-  digitalWrite(led3, HIGH);
-  delay(5000);// delay time 5s
-  digitalWrite(led3, LOW);
-  digitalWrite(led4, HIGH);
-  delay(8000);// delay time 8s
-  digitalWrite(led4, LOW);
-  digitalWrite(led2, HIGH);
-  delay(10000);// delay time 10s
-  digitalWrite(led2, LOW);
 
 }
